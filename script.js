d6943b9cd3d21d580d94751df477975436ec983b
Akshay S <akshaysclt2002@gmail.com>
2024-12-10 16:02:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,23 +43,30 @@ array.map(item => {
     }
 });
 
-// 3.[1, 2, 3, 4. 5, 6] : Write a closure function to delete indexes 5 and 2 of this array.
-const arr = [1, 2, 3, 4, 5, 6];
+// 3.[1, 2, 3, 4, 5, 6] : Write a closure function to delete indexes 5 and 2 of this array.
 
-function deleteIndex() {
-    arr.splice(5, 1);
-    arr.splice(2, 1);
-    console.log(arr);
+function main(){
+    const array=[1, 2, 3, 4, 5, 6];
+    function removeIndex(){
+        array.splice(5,1);
+        array.splice(2,1);
+        console.log(array);
+    }
+    return removeIndex;
 }
+let newarray=main();
+newarray();
 
-deleteIndex();
-
-// 4.Use constructor method to console all elements of the passed array. (the array will be passed to the constructor method)
-const array1 = ['car', 1, 5, 'js', 'node js', 'mysql', 'sql'];
-
-array1.forEach(item => {
-    console.log(item.constructor.name);
-});
+//4.Use constructor method to console all elements of the passed array. (the array will be passed to the constructor method)
+const array4=[15,62,53,75,97];
+class arrayPrint{
+    constructor(array4){
+        array4.forEach(num => {
+            console.log(num);
+        });
+    }
+}
+let new_array = new arrayPrint(array4);
 
 //5.Create a self-invoking function to console you name.
 (function () {
